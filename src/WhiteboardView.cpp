a10962a64b7738c0cd187e4ad5d1880668e17a38
pdkvi <pdkvi0@gmail.com>
2025-08-12 01:26:46 +0300

    Temporarily added debugging information about view and screen rects to WhiteboardView::paintEvent()

--- src/WhiteboardView.cpp
+++ src/WhiteboardView.cpp
@@ -87,6 +87,33 @@ void WhiteboardView::paintEvent(QPaintEvent* event)
 
 	painter.setPen(Qt::green);
 	painter.drawPolygon(mapFromScene(sceneRect()));
+
+	QRectF const viewRect = sceneRect();
+	auto const viewRectStr = QString("view rect: LEFT = %1, TOP = %2, BOTTOM = %3, RIGHT = %4")
+		.arg(viewRect.left())
+		.arg(viewRect.top())
+		.arg(viewRect.bottom())
+		.arg(viewRect.right());
+
+	QRectF const screenRect = mapToScene(QRect{0, 0, width(), height()}).boundingRect();
+	auto const screenRectStr = QString("screen rect: LEFT = %1, TOP = %2, BOTTOM = %3, RIGHT = %4")
+		.arg(screenRect.left())
+		.arg(screenRect.top())
+		.arg(screenRect.bottom())
+		.arg(screenRect.right());
+
+	QFont const monospace("Monospace", 10);
+	QFontMetricsF const fm(monospace);
+
+	painter.setPen(Qt::black);
+	painter.setFont(monospace);
+
+	QRect const viewStrRect = fm.boundingRect(viewRectStr).toRect();
+	painter.drawText((width() - viewStrRect.width()) / 2, viewStrRect.height(), viewRectStr);
+
+	painter.setPen(Qt::lightGray);
+	QRect const screenStrRect = fm.boundingRect(screenRectStr).toRect();
+	painter.drawText((width() - screenStrRect.width()) / 2, viewStrRect.height() + screenStrRect.height(), screenRectStr);
 }
 
 void WhiteboardView::resizeEvent(QResizeEvent* event)
