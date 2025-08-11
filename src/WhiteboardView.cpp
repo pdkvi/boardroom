1da86a800da1851326669e091c8deab4dc1c372c
pdkvi <pdkvi0@gmail.com>
2025-08-12 01:32:02 +0300

    Add infinite scene scrolling to the WhiteboardView widget

--- src/WhiteboardView.cpp
+++ src/WhiteboardView.cpp
@@ -144,10 +144,35 @@ void WhiteboardView::wheelEvent(QWheelEvent* event)
 		offset *= (event->angleDelta().y() > 0.0 ? 0.1 : -0.1);
 		offset = mapToScene(wndSceneCenter + offset.toPoint());
 
+		QRectF const viewRect = sceneRect();
+		QRectF const screenRect = mapToScene(QRect{ 0, 0, width(), height() }).boundingRect();
+
 		if (event->modifiers().testFlag(Qt::ShiftModifier))
+		{
+			double const leftOffset = -std::max(offset.x(), 0.0);
+			double const rightOffset = -std::min(offset.x(), 0.0);
+
+			if (screenRect.left() + leftOffset < viewRect.left() ||
+				screenRect.right() + rightOffset > viewRect.right())
+			{
+				setSceneRect(viewRect.adjusted(leftOffset, 0.0, rightOffset, 0.0));
+			}
+
 			translate(offset.x(), 0.0);
+		}
 		else
+		{
+			double const topOffset = -std::max(offset.y(), 0.0);
+			double const bottomOffset = -std::min(offset.y(), 0.0);
+
+			if (screenRect.top() + topOffset < viewRect.top() ||
+				screenRect.bottom() + bottomOffset > viewRect.bottom())
+			{
+				setSceneRect(viewRect.adjusted(0.0, topOffset, 0.0, bottomOffset));
+			}
+
 			translate(0.0, offset.y());
+		}
 	}
 }
 
@@ -185,6 +210,24 @@ void WhiteboardView::mouseMoveEvent(QMouseEvent* event)
 		QPointF const mappedLastMousePos = mapToScene(m_lastMousePos);
 
 		QPointF const offset = mappedMousePos - mappedLastMousePos;
+
+		QRectF const viewRect = sceneRect();
+		QRectF const screenRect = mapToScene(QRect{ 0, 0, width(), height() }).boundingRect();
+
+		double const leftOffset = -std::max(offset.x(), 0.0);
+		double const rightOffset = -std::min(offset.x(), 0.0);
+
+		double const topOffset = -std::max(offset.y(), 0.0);
+		double const bottomOffset = -std::min(offset.y(), 0.0);
+
+		if (screenRect.left() + leftOffset < viewRect.left() ||
+			screenRect.right() + rightOffset > viewRect.right() ||
+			screenRect.top() + topOffset < viewRect.top() ||
+			screenRect.bottom() + bottomOffset > viewRect.bottom())
+		{
+			setSceneRect(viewRect.adjusted(leftOffset, topOffset, rightOffset, bottomOffset));
+		}
+
 		translate(offset.x(), offset.y());
 
 		m_lastMousePos = mousePos;
