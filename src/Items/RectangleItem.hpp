b626b17c9ee3ad8ffdd91137fab63425eb5806bf
pdkvi <pdkvi0@gmail.com>
2025-09-01 23:27:26 +0300

    Add the ShapeTool and the first mode to it - RectangleItem.

---
+++ src/Items/RectangleItem.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Core/ToolItemBase.hpp"
+
+class RectangleItem : public ToolItemBase<RectangleItem>
+{
+	using base_t = ToolItemBase;
+	using this_t = RectangleItem;
+
+private:
+	QPointF m_startPt;
+	QPointF m_endPt;
+
+public:
+	QString getName() const override;
+
+protected:
+	void startPathImpl(QPointF const& pos) override;
+	void updatePathImpl(QPointF const& pos) override;
+	void endPathImpl(QPointF const& pos) override;
+
+	QRectF boundingRectImpl() const override;
+	void paintImpl(QPainter* painter, QStyleOptionGraphicsItem const* option, QWidget* widget) override;
+};
---
+++ src/Tools/ShapeTool.cpp
@@ -0,0 +1,26 @@
+﻿#include "ShapeTool.hpp"
+
+#include "Items/RectangleItem.hpp"
+#include "Items/CircleItem.hpp"
+
+#include "Utility/SingleCall.hpp"
+
+ShapeTool::ShapeTool()
+{
+	SingleCall<this_t>::run([]()
+	{
+		s_supportedItemsId.emplace(RectangleItem::getToolItemId());
+	});
+
+	setCurrentItemId(RectangleItem::getToolItemId());
+}
+
+QString ShapeTool::getName() const
+{
+	return "Shape tool";
+}
+
+QIcon ShapeTool::getIcon() const
+{
+	return QIcon{};
+}
