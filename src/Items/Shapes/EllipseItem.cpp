b619f03689b9a35157a68b876a22e34121118101
pdkvi <pdkvi0@gmail.com>
2025-09-03 01:24:20 +0300

    Extract the start and end points in ToolItemBase

---
+++ src/Items/Shapes/EllipseItem.cpp
@@ -0,0 +1,33 @@
+﻿#include "EllipseItem.hpp"
+
+#include <QPainter>
+
+QString EllipseItem::getName() const
+{
+	return "Ellipse";
+}
+
+QRectF EllipseItem::getLimitRect() const
+{
+	auto const [x1, y1] = getStartPathPt();
+	auto const [x2, y2] = getCurrentPathPt();
+
+	QPointF const topLeft = { std::min(x1, x2), std::min(y1, y2) };
+	QPointF const bottomRight = { std::max(x1, x2), std::max(y1, y2) };
+
+	return QRectF(topLeft, bottomRight);
+}
+
+void EllipseItem::onPaint(QPainter* painter, QStyleOptionGraphicsItem const* option, QWidget* widget)
+{
+	painter->setPen(QPen(Qt::black, 1));
+
+	QRectF const ellipseRect = [&]
+	{
+		qreal const halfPenWidth = painter->pen().widthF() / 2;
+		return getLimitRect().adjusted(halfPenWidth, halfPenWidth, -halfPenWidth, -halfPenWidth);
+	}();
+
+	painter->setClipRect(getLimitRect());
+	painter->drawEllipse(ellipseRect);
+}
---
+++ src/Items/Shapes/TriangleItem.cpp
@@ -0,0 +1,33 @@
+﻿#include "TriangleItem.hpp"
+
+#include <QVector2D>
+#include <QPainter>
+
+QString TriangleItem::getName() const
+{
+	return "Triangle";
+}
+
+QRectF TriangleItem::getLimitRect() const
+{
+	auto const [x1, y1] = getStartPathPt();
+	auto const [x2, y2] = getCurrentPathPt();
+
+	QPointF const topLeft = { std::min(x1, x2), std::min(y1, y2) };
+	QPointF const bottomRight = { std::max(x1, x2), std::max(y1, y2) };
+
+	return QRectF(topLeft, bottomRight);
+}
+
+void TriangleItem::onPaint(QPainter* painter, QStyleOptionGraphicsItem const* option, QWidget* widget)
+{
+	QRectF const& rect = getLimitRect();
+
+	QPainterPath path;
+	path.moveTo(rect.bottomLeft());
+	path.lineTo(rect.left() + rect.width() / 2, rect.top());
+	path.lineTo(rect.bottomRight());
+	path.lineTo(rect.bottomLeft());
+
+	painter->drawPath(path);
+}
