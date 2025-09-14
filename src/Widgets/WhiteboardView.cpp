99e8313200829229a0e51e574f797b250f8a30f5
Ilya Podkovyrin <pdkvi0@gmail.com>
2025-09-14 12:43:15 +0300

    Prevent the minimap from being disabled in interactive mode (#29)

--- src/Widgets/WhiteboardView.cpp
+++ src/Widgets/WhiteboardView.cpp
@@ -91,7 +91,8 @@ void WhiteboardView::renderDebugInformation(QPainter& painter) const
 
 void WhiteboardView::paintEvent(QPaintEvent* event)
 {
-	m_minimap->setVisible(!m_minimap->isTargetSceneFitInTargetView());
+	m_minimap->setVisible(m_state == CurrentState::Moving ||
+		!m_minimap->isTargetSceneFitInTargetView());
 
 	base_t::paintEvent(event);
 
